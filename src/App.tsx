e1a280c594dee17e31c75f06a1d9dd653dbe0aae
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-10-10 23:39:44 -0300

    M8 L3: Creatomg the context and provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ShowData } from './Pages/ShowData';
 import { SignUp } from './Pages/SignUp';
+import { ContextProvider } from './Contexts/Context';
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <h1>Título da Página</h1>
-            <hr />
+        <ContextProvider>
+            <BrowserRouter>
+                <h1>Título da Página</h1>
+                <hr />
 
-            <Routes>
-                <Route path="/" element={<SignUp />} />
-                <Route path="/exibir" element={<ShowData />} />
-            </Routes>
+                <Routes>
+                    <Route path="/" element={<SignUp />} />
+                    <Route path="/exibir" element={<ShowData />} />
+                </Routes>
         </BrowserRouter>
+        </ContextProvider>
     )
 }
 
---
+++ src/Contexts/Context.tsx
@@ -0,0 +1,23 @@
+import { createContext } from 'react';
+
+type ContextType = {
+    name: string;
+    age: number;
+}
+
+interface IContextProps {
+    children: React.ReactNode;
+}
+
+const initialState = {
+    name: 'Vinicius',
+    age: 21
+}
+
+const Context = createContext<ContextType>(initialState);
+
+export const ContextProvider: React.FC<IContextProps> = ({children}) => {
+    return (
+        <Context.Provider value={initialState}>{children}</Context.Provider>
+    )
+}
