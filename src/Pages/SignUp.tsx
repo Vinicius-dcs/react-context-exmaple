e5bbf9419c5523e4f24a03bdae465be91b463aa9
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-10-12 13:44:24 -0300

    M8 L7: Creating Context with Reducer

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -3,11 +3,21 @@ import { useContext } from 'react';
 import { Context } from '../Contexts/Context';
 
 export const SignUp = () => {
-    const { name, age } = useContext(Context);
+    const { state, dispatch } = useContext(Context);
+
+    const handleChangeName = () => {
+        dispatch({
+            type: 'CHANGE_NAME',
+            payload: {
+                name: 'Costa'
+            }
+        })
+    }
 
     return (
         <div>
-            Tela SignUp de {name} que tem {age} anos.
+            Tela SignUp de {state.user.name} que tem {state.user.age} anos.
+            <button onClick={handleChangeName}>Trocar nome para Costa</button>
             <br />
             <Link to="/exibir">Ir para ShowData</Link>
         </div>
